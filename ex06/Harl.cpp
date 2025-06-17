ab9ad1ce83e070bd2ceae76ac2c8168e52f0f017
Pierre Pontet <ppontet@student.42lyon.fr>
2025-06-17 14:42:13 +0200

    fix: reversed message order

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ppontet <ppontet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:05:58 by ppontet           #+#    #+#             */
-/*   Updated: 2025/03/02 17:10:38 by ppontet          ###   ########lyon.fr   */
+/*   Updated: 2025/06/17 14:40:32 by ppontet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,27 +55,26 @@ void Harl::moreComplaints(std::string input)
 	switch(index)
 	{
 		case 4:
-			this->complain("NOT_DEFINED");
 			(this->*f[4])();
 			break;
-		case 3:
-			std::cout << "[ " << str[3] << " ]" << std::endl;
-			(this->*f[3])();
-		case 2:
-			std::cout << "[ " << str[2] << " ]" << std::endl;
-			(this->*f[2])();
-		case 1:
-			std::cout << "[ " << str[1] << " ]" << std::endl;
-			(this->*f[1])();
 		case 0:
 			std::cout << "[ " << str[0] << " ]" << std::endl;
 			(this->*f[0])();
+		case 1:
+			std::cout << "[ " << str[1] << " ]" << std::endl;
+			(this->*f[1])();
+		case 2:
+			std::cout << "[ " << str[2] << " ]" << std::endl;
+			(this->*f[2])();
+		case 3:
+			std::cout << "[ " << str[3] << " ]" << std::endl;
+			(this->*f[3])();
 		default:
 			break;
 	}
 }
 
-	void Harl::debug(void)
+void Harl::debug(void)
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
